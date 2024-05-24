2e36634f6cce7410409d458035c3e2a47877ae23
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-05-24 19:12:59 +0900

    コマンドライン引数をチェック
    (1) ダウンロードするバージョンを指定

--- download-installer.sh
+++ download-installer.sh
@@ -4,3 +4,10 @@ script_file=${BASH_SOURCE:-$0}
 script_dir=$(readlink -f "$(dirname "${script_file}")")
 
 source  "${script_dir}/config.rc"
+
+##################################################################
+##
+##    1.  引数チェック
+##
+
+target_version=$1
